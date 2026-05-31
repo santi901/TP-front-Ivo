0cbb2e96f42e8ce28d4b5b8965d9c4fb0ccdb629
nacho883 <bourdieu.ignacio0@gmail.com>
2026-05-31 20:10:28 -0300

    creo que esta listo

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zwyhgkdbkjojwebrvyuc.supabase.co';
-const supabaseKey = 'sb_publishable_B7dAu9sGtNHw96uWODSf5A_35PKXHG9';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  'https://zwyhgkdbkjojwebrvyuc.supabase.co',
+  'sb_publishable_B7dAu9sGtNHw96uWODSf5A_35PKXHG9'
+);
